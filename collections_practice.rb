4195b086a12089d523500d20c9d10f73aa842d1b
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-12 20:20:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,13 @@ end
 
 # replace third character in a string with $
 def kesha_maker(array)
-  
+    kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
 end
 
 def sum_array
