282e5df6883182cfa9e714efaf4e6a6dd48223f3
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-12 19:53:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc
+  
+end
+
+def sort_array_desc
+  
+end
+
+def sort_array_char_count
+  
+end
+
+def swap_elements
+  
+end
+
+def reverse_array
+  
+end
+
+def kesha_maker
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
 
